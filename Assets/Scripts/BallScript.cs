48f5539a3f0aa20e576db9b362712f569db753fc
logonrm <logonrm@fiap.com.br>
2018-02-17 14:37:21 -0200

    Criando Scene de game

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+	private bool isBallActive;
+	private Vector3 ballPosition;
+	private Vector2 ballInitialForce;
+	private Rigidbody2D rb;
+
+	public GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+		ballInitialForce = new Vector2 (100.0f, 300.0f);
+		isBallActive = false;
+		ballPosition = transform.position;
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump") && !isBallActive) {
+			rb.AddForce (ballInitialForce);
+			isBallActive = true;
+		}
+
+		if (!isBallActive && playerObject != null) {
+			ballPosition.x = playerObject.transform.position.x;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	public float velocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float px = Input.GetAxisRaw ("Horizontal") * velocity * Time.deltaTime;
+
+		transform.Translate (px, 0.0f, 0.0f);
+	}
+}
